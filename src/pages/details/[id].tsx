f6981075e8873bccae7f60fc71da1548121228a6
Dave P <davep1405@gmail.com>
2023-04-23 19:53:08 +0700

    added try catch for potential static page error

--- src/pages/details/[id].tsx
+++ src/pages/details/[id].tsx
@@ -1,12 +1,22 @@
 import DetailsCard from "@/components/cards/details_card";
 import Template from "@/components/template";
 import { CharacterObject } from "@/lib/character";
+import { ThemeContext } from "@/lib/theme_context";
 import { useRouter } from "next/router";
+import { useContext } from "react";
 
 function ViewDetails() {
     const router = useRouter();
     let id = router.query.id;
-    let data: CharacterObject = JSON.parse(Object.keys(router.query)[0]);
+    let data: CharacterObject;
+
+    let theme = useContext(ThemeContext);
+
+    try {
+        data = JSON.parse(Object.keys(router.query)[0]);
+    } catch(e) {
+        return <p className={`${theme.textColor}`}>Loading...</p>
+    }
 
     if(id === undefined) return <p>Loading</p>
     return (
